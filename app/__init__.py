7f1d821f02c52b90babf5d6a714b80bca9c1a829
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-06-17 16:40:38 -0700

    Add work experience section using Jinja (closes #5)

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    work_experience = [
+        {
+            "company": "City College of San Francisco",
+            "role": "Lead Full Stack Software Engineer",
+            "duration": "Feb 2025 - Jun 2025",
+            "description": "Built Zoom automation tools using Electron + React, improving session start times by 99.9%."
+        },
+        {
+            "company": "Mermory",
+            "role": "Frontend Developer Intern",
+            "duration": "Apr 2025 - May 2025",
+            "description": " Partnered with UI/UX designers to translate Figma prototypes into a front-end landing page in code within 2 days"
+        }
+    ]
+    return render_template('index.html', work_experience=work_experience, title="MLH Fellow", url=os.getenv("URL"))
