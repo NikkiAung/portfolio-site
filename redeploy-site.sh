99e0ce7be2ffc0749f520c0eb60bd8890a3741fb
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-07-24 01:30:02 -0700

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -4,16 +4,10 @@
 cd /portfolio-site || exit
 
 # 2. Make sure we have the latest code from GitHub
-git fetch
-git reset origin/main --hard
+git fetch && git reset origin/main --hard
 
-# 3. Enter the virtual environment
-source python3-virtualenv/bin/activate
+# 3. Clean up any old docker-compose.prod.yml
+docker compose -f docker-compose.prod.yml down
 
-# 4. Install any new python dependencies
-pip install -r requirements.txt
-
-# 5. Restart the systemd service (replace 'myportfolio' with your actual service name if different)
-sudo systemctl restart my-portfolio
-
-echo "Site redeployed and service restarted! :)"
+# 4. Build the Docker images
+docker compose -f docker-compose.prod.yml up -d --build
